bd2d833bb64ed4ce51230fb5b691fb027ddc7b23
carlo <carlo@DESKTOP>
2023-10-11 14:28:48 -0600

    Add project files.

---
+++ MinimalAPIPostgreSQLEF/Data/OfficeDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIPostgreSQLEF.Models;
+
+namespace MinimalAPIPostgreSQLEF.Data
+{
+    public class OfficeDb: DbContext
+    {
+        public OfficeDb(DbContextOptions<OfficeDb> options) : base(options)
+        {
+            
+        }
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
